6f3473067608ea57623227ea229177a86ed5275b
mahtab <mahtabasadi.job@gmail.com>
2023-02-07 08:18:28 +0330

    index.js changed in signUp branch

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ alert("hi man...")
 console.log('good morning');
 console.log("my friend");
 console.log("my life");
+
+// function .......
+
+console.log('hi');
+console.log('mahtab');
+console.log('mahtab mim !!! .........');
