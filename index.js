5df6ba3e44dd8f7ed1a5b2a36c3dec25221511ed
mahtab <mahtabasadi.job@gmail.com>
2023-02-07 08:22:11 +0330

    master changed ...

--- index.js
+++ index.js
@@ -3,3 +3,13 @@ alert("hi man...")
 console.log('good morning');
 console.log("my friend");
 console.log("my life");
+
+
+// function .......
+
+console.log('hi');
+console.log('mahtab');
+console.log('mahtab mim !!! .........');
+console.log('mahtab mim !!! .........');
+console.log('mahtab mim !!! .........');
+console.log('mahtab mim !!! .........');
